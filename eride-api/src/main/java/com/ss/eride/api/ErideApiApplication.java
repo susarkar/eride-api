266bf1369f982b2557810636a4d5468e0e8c663e
Skybit <Susanta>
2019-11-22 23:00:23 +0530

    first comitt

---
+++ eride-api/src/main/java/com/ss/eride/api/ErideApiApplication.java
@@ -0,0 +1,13 @@
+package com.ss.eride.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErideApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErideApiApplication.class, args);
+	}
+
+}
---
+++ eride-api/src/test/java/com/ss/eride/api/ErideApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.eride.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErideApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
